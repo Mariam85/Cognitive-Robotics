98583957f0a40ed50a211f8c5366e7e557b75e81
khadijaswelam <57529975+khadijaswelam@users.noreply.github.com>
2022-12-08 23:28:29 +0200

    Update Controls

--- src/robotControl_pkg/script/controls.py
+++ src/robotControl_pkg/script/controls.py
@@ -1,62 +1,77 @@
 #!/usr/bin/env python3
 import rospy
 
-import keyboard
+#import keyboard
+from pynput import keyboard
 from rospy.rostime import Time
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
+letter = 'm'
+
+def on_press(key):
+    global letter 
+    letter = key.char
+    print(letter)
+
+def on_release(key):
+    global letter
+    letter = 'm'
+
 def main():
     # Initialize Node with node name
-    rospy.init_node('hello_controller')
+    rospy.init_node('controls')
     # Assign node as a publisher to this topic
-    pub = rospy.Publisher('/robot/robotnik_base_control/cmd_vel
-', Twist, queue_size=100)
+    pub = rospy.Publisher('/robot/robotnik_base_control/cmd_vel', Twist, queue_size=100)
     # Get the current time in seconds
     start = Time.now().to_sec()
-
     vel_msg = Twist()
     maxSpeed = 10
-    angularSpeed_d = 5 #degrees/sec
+    angularSpeed_d = 1 #degrees/sec
     angularSpeed_r = (angularSpeed_d * 3.14) / 180 
     
+    listener = keyboard.Listener(on_press=on_press , on_release= on_release)
+    listener.start()
+
     while not rospy.is_shutdown():
-
-        if keyboard.is_pressed('w') or keyboard.is_pressed('W') :
+        print('here')
+        print('lettter '+letter+' in while loopppp')
+        #if keyboard.is_pressed('w') or keyboard.is_pressed('W') :
+        if letter =='w' or letter =='W' :
             # ACCELERATE
-            if vel_msg.linear.y < maxSpeed:
-                vel_msg.linear.y += 0.5
+            print("enter acceleration")
+            if vel_msg.linear.x < maxSpeed:
+                vel_msg.linear.x += 0.1
         
-        if keyboard.is_pressed('s') or keyboard.is_pressed('S') :
+        #if keyboard.is_pressed('s') or keyboard.is_pressed('S') :
+        if letter =='s' or letter =='S' :
             # DECELERATE
-            if vel_msg.linear.y > 0:
-                vel_msg.linear.y -= 0.3
+            if vel_msg.linear.x > 0:
+                vel_msg.linear.x -= 0.05
 
-        if keyboard.is_pressed('a') or keyboard.is_pressed('A') :
-            # LEFT -- anticlockwise
+        #if keyboard.is_pressed('a') or keyboard.is_pressed('A') :
+        if letter =='a' or letter =='A' :
+           # LEFT -- anticlockwise
             vel_msg.angular.z += angularSpeed_r
 
 
-        if keyboard.is_pressed('d') or keyboard.is_pressed('D') :
+        #if keyboard.is_pressed('d') or keyboard.is_pressed('D') :
+        if letter =='d' or letter =='D' :
+            print("enter right")
+
             # RIGHT -- clockwise
             vel_msg.angular.z -= angularSpeed_r
 
 
-        if not (keyboard.is_pressed('w') or keyboard.is_pressed('W')) and not (keyboard.is_pressed('s') or keyboard.is_pressed('S')):
+        #if not (keyboard.is_pressed('w') or keyboard.is_pressed('W')) and not (keyboard.is_pressed('s') or keyboard.is_pressed('S')):
+        if not(letter =='w' or letter =='W') and not(letter == 's' or letter == 'S') :
             # DECELERATE WITH TIME PASSING 
-            if vel_msg.linear.y > 0:
-                vel_msg.linear.y -= 0.1
+            if vel_msg.linear.x > 0:
+                vel_msg.linear.x -= 0.02
 
 
 
-        # If 5 seconds are passed
-        if Time.now().to_sec() - start >= 5:
-            msg = String()
-            start = Time.now().to_sec()
-            msg.data = 'Hello world at {}'.format(start)
-            # Broadcast the message to the topic hello_sender
-            pub.publish(msg)
-            print('msg sent')
+        pub.publish(vel_msg)
 
 if __name__ == '__main__':
     try:
